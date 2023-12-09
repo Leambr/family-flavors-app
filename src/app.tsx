f1e787d8fb65d88048e2dfba8f6108bb4c65da3a
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2023-12-09 22:03:08 +0100

    chore: add css files

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
+import './design-system/styles/variables/typography.css';
+import './design-system/styles/variables/colors.css';
+import './design-system/styles/variables/spaces.css';
+import './design-system/styles/reset.global.css';
 
 // App Component
 const App = () => (
     <div>
         <h1>Hello, test to see if esbuild workhfhs oui ça marche trop biennnnn</h1>
+        <p>Test</p>
+        <p>Ca marche encore</p>
     </div>
 );
 
