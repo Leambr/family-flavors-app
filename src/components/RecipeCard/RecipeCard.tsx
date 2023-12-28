0d63a37d98a18b597e67d9c6a09cc03a83091018
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2023-12-28 23:07:10 +0100

    feat: name and description responsive

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import cx from 'classnames';
+import React, { useEffect, useState } from 'react';
 import { Card } from '../Card/Card';
 import s from './RecipeCard.module.css';
 
@@ -10,16 +11,38 @@ interface RecipeCardProps {
 }
 
 export const RecipeCard = ({ imageUrl, recipeName, description, dietType }: RecipeCardProps) => {
+    const [screenSize, setScreenSize] = useState<number>(window.innerWidth);
+    const isScreenSmall = screenSize < 720;
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); //
+
     return (
         <Card className={s.cardWrapper}>
             <div className={s.imageWrapper}>
                 <img src={imageUrl} className={s.image} alt="Pasta Recipe" />
+                {isScreenSmall && (
+                    <div className={s.nameOverlay}>
+                        <p className={s.recipeName}>{recipeName}</p>
+                    </div>
+                )}
             </div>
-            <div className={s.nameAndDescription}>
-                <p className={s.dietType}>{dietType}</p>
-                <p className={s.recipeName}>{recipeName}</p>
-                <p className={s.description}>{description}</p>
-            </div>
+            {!isScreenSmall && (
+                <div className={s.nameAndDescription}>
+                    <p className={s.dietType}>{dietType}</p>
+                    <p className={s.recipeName}>{recipeName}</p>
+                    <p>{description}</p>
+                </div>
+            )}
         </Card>
     );
 };
