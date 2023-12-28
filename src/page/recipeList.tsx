97886de3e731300fd7ab35ea7ba26a2823b8c200
Léa Mhoumadi <mhoumadi.lea@gmail.com>
2023-12-28 11:11:39 +0100

    feat: add margin for each device sizes

--- src/page/recipeList.tsx
+++ src/page/recipeList.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { RecipeCard } from '../components/RecipeCard/RecipeCard';
-import s from './recipeList.module.css';
 import { Carousel } from '../components/Carousel/Carousel';
-import pasta from '../assets/pasta.jpg';
+import s from './recipeList.module.css';
 
 export const RecipeList = () => {
     return (
         <>
-            <section className="mb-104">
+            <section className="mb-40 mb-72 mb-104">
                 <h2 className={s.title}>A base de pâtes</h2>
                 {/* voir plus button */}
 
                 <Carousel />
             </section>
-            <section className="mb-104">
+            <section className="mb-40 mb-72 mb-104">
                 <h2 className={s.title}>A base de pâtes</h2>
                 {/* voir plus button */}
 
                 <Carousel />
             </section>
-            <section className="mb-104">
+            <section className="mb-40 mb-72 mb-104">
                 <h2 className={s.title}>A base de pâtes</h2>
                 {/* voir plus button */}
 
