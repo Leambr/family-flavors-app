c89a52a49db3f739dacc039f04be938f8536ee22
Léa Mhoumadi <95865130+Leambr@users.noreply.github.com>
2024-04-16 17:21:00 +0200

    fix: html error in public folder (#3)

--- esbuild/serve.js
+++ esbuild/serve.js
@@ -1,11 +1,27 @@
 import esbuild from 'esbuild';
 import { createBuildSettings } from './settings.js';
+import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
+import CssModulesPlugin from 'esbuild-css-modules-plugin';
 
 const settings = createBuildSettings({
     sourcemap: true,
     banner: {
         js: `new EventSource('/esbuild').addEventListener('change', () => location.reload())`,
     },
+    plugins: [
+        htmlPlugin({
+            files: [
+                {
+                    entryPoints: ['src/app.tsx'],
+                    filename: 'index.html',
+                    htmlTemplate: './index.html',
+                },
+            ],
+        }),
+        CssModulesPlugin({
+            v2: true,
+        }),
+    ],
 });
 
 const ctx = await esbuild.context(settings);
